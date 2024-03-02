3560d9f7f0a80499428404c88e88a927c559dd08
oscarlb1 <161454630+oscarlb1@users.noreply.github.com>
2024-03-02 04:08:05 +0100

    Update index.js

--- Calculadora_Js/index.js
+++ Calculadora_Js/index.js
@@ -1,22 +1,25 @@
-// CALCULATOR PROGRAM
-const display = document.getElementById("display"); // Coge el ID del display. Es donde se mostrará los números que seleccionas.
+// CALCULATOR PROGRAM 
 
-// FUNCION PARA QUE APAREZCAN LOS VALORES AL CLICKARLOS
+// DECLARAS LA VARIABLE DISPLAY, LA CUAL VA A COGER LAS ID'S DE LOS BOTONES QUE PULSES
+const display = document.getElementById("display"); // Te coge los ID de los elementos de dentro del display.
+
+// FUNCION PARA QUE SE MUESTREN LOS NUMEROS ARRIBA
 function appendToDisplay(input) {
-    display.value += input; // Hace que el boton que cliques aparezca en la barra de arriba de la calculadora.
+    display.value += input; // Te hace que al clicar un número de la calculadora u operación te lo escriba en el display de arriba.
 }
 
-// FUNCION PARA LIMPIAR LOS VALORES
-function clear() {
-    display.value = ""; // Hace que el valor arriba sea "" por lo que borra lo que haya puesto.
+// FUNCION PARA LIMPIAR LA PANTALLA
+function clearDisplay() {
+    display.value = ""; // Para borrar los datos del display de arriba y limpiar las operaciones.
 }
 
-// FUNCION PARA CALCULAR  LA OPERACIÓN EN EL DISPLAY
-function calculate() {
+// FUNCION DE LA CALCULADORA
+function calcular() {
     try {
-        display.value = eval(display.value); // Te resuelve los valores que hay en el display.
+        display.value = eval(display.value); // Coge el valor que haya en el display y lo evalua, es decir que te lo calcula, y luego te devuelve el valor.
     }
     catch (error) {
-        display.value = "Error." // Si  no se puede resolver la operación te salta un mensaje de error.
+        display.value = "Error"; // Si salta algún error o no se puede completar la operación, te sale un mensaje de error,
+        display.value = "";     //  y te limpia la pantalla de nuevo.
     }
 }
